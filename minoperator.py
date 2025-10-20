0929237eb14509d4792eca2c10f48d65d2a58814
Vyas Ved Himanshu <vedcodesgithubid22312@gmail.com>
2025-10-20 22:43:25 +0530

    Create minoperator.py

---
+++ minoperator.py
@@ -0,0 +1,11 @@
+#finds the minimum element of the sequence passed as an argument
+# Demonstrating min() - Example 1:
+print(min("aAbByYzZ"))
+
+
+# Demonstrating min() - Examples 2 & 3:
+t = 'The Knights Who Say "Ni!"'
+print('[' + min(t) + ']')
+
+t = [0, 1, 2]
+print(min(t))
