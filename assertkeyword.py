310c439fdc2235af1a0bdd36e34c8a12f46cf487
Vyas Ved Himanshu <vedcodesgithubid22312@gmail.com>
2025-10-20 22:52:07 +0530

    Create assertkeyword.py

---
+++ assertkeyword.py
@@ -0,0 +1,17 @@
+#The assert keyword in Python is used for debugging and testing — it helps verify that a certain condition in your code is true at runtime.
+""" 
+x = -5
+assert x > 0, "x must be positive"
+print("x is positive")
+
+"""
+
+import math
+
+x = float(input("Enter a number: "))
+assert x >= 0.0
+
+x = math.sqrt(x)
+
+print(x)
+    
